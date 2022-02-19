37deb576eafa10cad6bb9ead909ec5b79b28c88c
Pavel Kubiš <vcpjoz22@gmail.com>
2022-02-19 16:25:52 +0100

    Prijem
    Zrušeno zobrazování polí, které nejsou nutné pro výběr součástky

--- PPPart/prijem_novy_1.cpp
+++ PPPart/prijem_novy_1.cpp
@@ -6,6 +6,34 @@ Prijem_novy_1::Prijem_novy_1(QWidget *parent) :
     ui(new Ui::Prijem_novy_1)
 {
     ui->setupUi(this);
+
+    ui->Umisteni->hide();
+    ui->Kategorie->hide();
+    ui->Dodavatel->hide();
+    ui->Vyrobce->hide();
+    ui->Poznamka->hide();
+    ui->Stav->hide();
+    ui->Cena->hide();
+    ui->Polozek->hide();
+    ui->Min_sklad->hide();
+    ui->Delka->hide();
+    ui->Pouzdro->hide();
+    ui->Tolerance->hide();
+    ui->Hmotnost->hide();
+    ui->Hodnota->hide();
+    ui->Koncovka_1->hide();
+    ui->Koncovka_2->hide();
+    ui->Zaporna_tolerance->hide();
+    ui->Kladna_tolerance->hide();
+    ui->Poc_vodicu->hide();
+    ui->Material->hide();
+    ui->Kapacita->hide();
+    ui->Jme_vykon->hide();
+    ui->Jme_napeti->hide();
+    ui->Ozn_knihovna->hide();
+    ui->Baleni->hide();
+    ui->Int_oznaceni->hide();
+
 }
 
 Prijem_novy_1::~Prijem_novy_1()
